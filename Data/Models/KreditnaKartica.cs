bf5b88a73d158581f870b12dbffc256efe978318
Mirza Rujanac <mirzarujanac2@gmail.com>
2024-01-11 17:12:51 +0100

    Dodan [Key] iznad KarticaID

--- Data/Models/KreditnaKartica.cs
+++ Data/Models/KreditnaKartica.cs
@@ -1,7 +1,10 @@
-﻿namespace StayHard.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StayHard.Data.Models
 {
     public class KreditnaKartica
     {
+        [Key]
         public int KarticaID { get; set; }
         public string BrojKartice { get; set; }
         public DateTime DatumIsteka { get; set; }
