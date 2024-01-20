16ab1d5a32d5a43339dae63002db5dffc35a3b3e
Mirza Rujanac <mirzarujanac2@gmail.com>
2024-01-20 19:46:40 +0100

    Dodani potrebni kontroleri/helperi za login

---
+++ BackEnd/Modules/Controllers/KorisnikController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StayHard.Data;
+using StayHard.Data.Models;
+using StayHard.Modules.ViewModels;
+
+namespace StayHard.Modules.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class KorisnikController: ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public KorisnikController(ApplicationDBContext dBContext)
+        {
+            this._dbContext = dBContext;
+        }
+
+        public Korisnik Registracija([FromBody] KorisnikRegistracijaVM x)
+        {
+            Korisnik? objekat;
+            if (x.KorisnikID == 0)
+            {
+                objekat = new Korisnik();
+                _dbContext.Add(objekat);
+            }
+            else
+            {
+                objekat = _dbContext.Korisnik.Find(x.KorisnikID);
+            }
+            objekat.KorisnickoIme = x.KorisnickoIme;
+            objekat.Email = x.Email;
+            objekat.Password = x.Password;
+
+            _dbContext.SaveChanges();
+            return objekat;
+        }
+    }
+}
---
+++ BackEnd/Modules/ViewModels/KorisnikRegistracijaVM.cs
@@ -0,0 +1,14 @@
+﻿using StayHard.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StayHard.Modules.ViewModels
+{
+    public class KorisnikRegistracijaVM
+    {
+        public int KorisnikID { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int BMI { get; set; }
+    }
+}
